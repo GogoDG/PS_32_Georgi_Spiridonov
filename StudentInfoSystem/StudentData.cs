ea8cff75f48f8df9a679ff3d38607d4de0bb221e
GogoDG <74346694+GogoDG@users.noreply.github.com>
2022-03-13 18:03:37 +0200

    Add files via upload

---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    internal class StudentData
+    {
+        public static List<Student> studentsArray;
+
+        public static List<Student> testStudents
+        {
+            get
+            {
+                return getStudents();
+            }
+            private set { }
+        }
+
+        public static List<Student> getStudents()
+        {
+            studentsArray = new List<Student>();
+
+            studentsArray.Add(new Student("Donovan", "Thomas", "Briggs", "FKST", "KSI", "bachelor", "semester graduate", "121218556", 4, 7, 31));
+            studentsArray.Add(new Student("Kiro", "Gavrailov", "Dondukov", "FPMI", "MI", "bachelor", "discontinued", "503119002", 1, 9, 22));
+            studentsArray.Add(new Student("Joseph", "Jorge", "Joestar", "FKST", "KSI", "bachelor", "active", "503119106", 3, 9, 50));
+
+            return studentsArray;
+        }
+    }
+}
