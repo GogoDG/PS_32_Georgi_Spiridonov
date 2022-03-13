1e2c7e0039da06b9ac8539b91beac10ada5615d8
GogoDG <74346694+GogoDG@users.noreply.github.com>
2022-03-13 18:01:07 +0200

    Add files via upload

---
+++ UserLogin/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    public static class Logger
+    {
+        private static List<string> currentSessionActivities = new List<string>();
+
+        static public void LogActivity(string activity)
+        {
+            string activityLine = DateTime.Now + ";" + LoginValidation.currentUserUsername + ";" + LoginValidation.currentUserRole + ";" + activity;
+            currentSessionActivities.Add(activityLine);
+
+            if (File.Exists("test.txt") == true)
+            {
+                File.WriteAllText("test.txt", activityLine);
+            }
+        }
+
+        static public void LogActivity(string activity, int errorCode)
+        {
+            string activityLine = DateTime.Now + ";" + errorCode + ";" + LoginValidation.currentUserRole + ";" + activity;
+            currentSessionActivities.Add(activityLine);
+
+            if (File.Exists("log.txt") == true)
+            {
+                File.WriteAllText("log.txt", activityLine);
+            }
+        }
+
+        static public IEnumerable<string> getCurrentSessionActivities(string filter)
+        {
+            List<string> filteredActivities = (from activity in currentSessionActivities where activity.Contains(filter) select activity).ToList();
+
+            return filteredActivities;
+        }
+    }
+}
---
+++ WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, RoutedEventArgs e)
+        {
+            string s;
+
+            if (txtName.Text.Length < 3)
+            {
+                MessageBox.Show("Name should be at least 2 characters long.");
+            }
+            else
+            {
+                // MessageBox.Show("Hello. " + txtName.Text + "\nThis is something, I guess...");
+                foreach (var item in mainGrid.Children)
+                {
+                    if (item is TextBox)
+                    {
+                        s = ((TextBox)item).Text;
+                        s = s + '\n';
+                        MessageBox.Show("Hello. " + s + "\nThis is something, I guess...");
+                    }
+                }
+            }
+
+        }
+    }
+}
